a7d6c68ae6c6131cfe28422502f5d2f650af1fcc
simarsher220 <simarsher220@gmail.com>
2020-09-09 10:27:38 +0530

    Initial commit

---
+++ restapi/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index():
+    return HttpResponse("Hello, world. You're at Rest.")
